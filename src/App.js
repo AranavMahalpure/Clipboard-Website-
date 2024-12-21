e49de22e899710645a1e72dda52492775e799d34
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-12-21 14:52:20 +0530

    updated version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import Message from  "./components/Message";
-import Login from './components/Login,js';
-import SignUp from './components/Signup';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
- 
-} from "react-router-dom";
+
 const App = () => {
   
   return (
     <div><Message/>
-       {/* <Router>
-        <Routes>
-      <Route path= "/Login" element={<Login/>}/> 
-      <Route path= "/Message" element={<Message/>}/> 
-      </Routes>
-     </Router> */}
     </div>
   );
 };
